826e6169ea79d0870e2c90c73b11ee0bd86d2d47
Gustavo Garcia <luctus@gmail.com>
2017-12-13 22:56:57 -0300

    ADD: learning about functions

--- car.js
+++ car.js
@@ -8,10 +8,16 @@ var Car = /** @class */ (function () {
     Car.prototype.stop = function () {
         console.log("Engine stopped: " + this.engine);
     };
+    Car.prototype.speed = function (finalSpeed) {
+        console.log("new speed: " + (finalSpeed || 10));
+    };
     return Car;
 }());
 window.onload = function () {
     var c = new Car("V8");
     c.start();
     c.stop();
+    c.speed();
+    c.speed(35);
+    c.speed();
 };
--- car.ts
+++ car.ts
@@ -9,10 +9,16 @@ class Car{
   stop(){
     console.log("Engine stopped: " + this.engine);
   }
+  speed(finalSpeed?: number): void {
+    console.log("new speed: " + (finalSpeed || 10));
+  }
 }
 
 window.onload = function() {
   var c = new Car("V8");
   c.start();
   c.stop();
+  c.speed();
+  c.speed(35);
+  c.speed();
 }
